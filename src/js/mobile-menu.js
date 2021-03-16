6775e6b6ecdaca73ae71a07ebe591682ce9dcc37
Nataliia Valko <tanarisoffice@gmail.com>
2021-03-16 21:05:44 +0200

    HEADER

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,9 +1,15 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const btnNoneRef = document.querySelector("[data-menu-none]");
+  const btnNoneSecondRef = document.querySelector("[data-menu-none-second]");
   const mobileBtnClose = document.querySelector("[data-menu-close]");
+
+
   menuBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
+    btnNoneRef.classList.toggle("is-none");
+    btnNoneSecondRef.classList.toggle("is-none");
   });
   menuBtnRef.addEventListener("click", toggleMenu);
   function toggleMenu() {
@@ -11,6 +17,8 @@
   }
   mobileBtnClose.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
+    btnNoneRef.classList.toggle("is-none");
+    btnNoneSecondRef.classList.toggle("is-none");
     document.body.classList.toggle("menu-open");
   });
 })();
