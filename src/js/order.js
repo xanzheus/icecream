1d933f191822d26ddfc977510b2144750c366680
Nataliia Valko <tanarisoffice@gmail.com>
2021-03-17 05:53:01 +0200

    prod

--- src/js/order.js
+++ src/js/order.js
@@ -2,12 +2,18 @@
   const refs = {
     openOrderBtn: document.querySelector('[data-order-open]'),
     openOrderSecBtn: document.querySelector('[data-order-open-second]'),
+    openOrder3Btn: document.querySelector('[data-order-open-3]'),
+    openOrder4Btn: document.querySelector('[data-order-open-4]'),
+    openOrder5Btn: document.querySelector('[data-order-open-5]'),
     closeOrderBtn: document.querySelector('[data-order-close]'),
     order: document.querySelector('[data-order]'),
   };
 
   refs.openOrderBtn.addEventListener('click', toggleOrder);
   refs.openOrderSecBtn.addEventListener('click', toggleOrder);
+  refs.openOrder3Btn.addEventListener('click', toggleOrder);
+  refs.openOrder4Btn.addEventListener('click', toggleOrder);
+  refs.openOrder5Btn.addEventListener('click', toggleOrder);
   refs.closeOrderBtn.addEventListener('click', toggleOrder);
 
   function toggleOrder() {
